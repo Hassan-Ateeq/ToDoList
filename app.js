1ccdafea5af1c5ca287ef4256646f708ae035760
Hassan Ateeq <hassanateeq44@gmail.com>
2020-05-31 22:06:10 +0500

    Did Some Logic

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+
+    const today = new Date();
+
+    const currentDay = today.getDay();
+
+    if(currentDay === 6 || currentDay === 0)
+    {
+        res.write("It's Time to Read some Books and do some code");
+    }
+    else{
+        res.write("It's time to Concentarte to University Studies");
+    }
+
+    res.send();
+});
+
+
+
+
+app.listen(process.env.PORT || 3000, ()=> console.log("Server is Running at 5000 Port"));
